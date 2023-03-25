992c4345c9838f0d956e07ee4afb9e5177c9b345
Cris Val <elgael06@gmail.com>
2023-03-25 15:23:19 -0700

    modificacion de carga principal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import ContactSection from './components/contact-section'
-import GallerySection from './components/gallery-section'
-import SeccionHero from './components/hero-section'
-import ServiceSection from './components/services-section'
+import { lazy, Suspense } from 'react';
 import './configs/firebase'
+
+const ContactSection = lazy(()=> import('./components/contact-section'));
+const GallerySection = lazy(()=> import('./components/gallery-section'));
+const SeccionHero = lazy(()=> import('./components/hero-section'));
+const ServiceSection = lazy(()=> import('./components/services-section'));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<div>Cargando...</div>} >
       <SeccionHero />
       <main id="main" data-aos="fade" data-aos-delay="1500">
         <GallerySection />
         <ServiceSection />
         <ContactSection />
        </main>
-      </>
+    </Suspense>
   )
 }
 
