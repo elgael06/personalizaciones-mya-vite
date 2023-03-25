822aad6cad91153302a0175271115f0552400811
Cris Val <elgael06@gmail.com>
2023-03-24 23:15:18 -0700

    logos y fotos de productos.

--- src/data/galeria.ts
+++ src/data/galeria.ts
@@ -1,11 +1,11 @@
 import IGallery from "../interfaces/IGallery.interface";
 
 export const galleryList: IGallery[] = [
-  { image: "https://www.fotoplus2000.com/wp-content/uploads/2017/04/Playera.png", name: 'payeras' },
-  { image: "https://http2.mlstatic.com/D_NQ_NP_127905-MLM25084289446_102016-O.jpg", name: 'cachuchas' },
-  { image: "https://http2.mlstatic.com/D_NQ_NP_2X_913584-MLM44825396633_022021-F.webp", name: 'placa' },
-  { image: "https://http2.mlstatic.com/D_NQ_NP_2X_859917-MLM53384513131_012023-F.webp", name: 'portaretratos' },
-  { image: "https://i2.wp.com/www.tuproductovirtual.com/wp-content/uploads/2021/09/Unicornio1.jpg?fit=595%2C595&ssl=1", name: 'tazas' },
-  { image: "https://http2.mlstatic.com/D_NQ_NP_951244-MLM45575307413_042021-O.jpg", name: 'llaveros' },
-  { image: "https://ae01.alicdn.com/kf/S70032da91ae34fe8ada365675ea39fcd1/Plumas-de-sublimaci-n-de-8-piezas-con-envoltura-retr-ctil-Pluma-de-transferencia-de-calor.jpg_Q90.jpg_.webp", name: 'pluma' },
+  { image: "assets/img/gallery/productos/Playera.png", name: 'payeras' },
+  { image: "assets/img/gallery/productos/cachucha.png", name: 'cachuchas' },
+  { image: "assets/img/gallery/productos/llaveros.png", name: 'llaveros' },
+  { image: "assets/img/gallery/productos/placa_perro.png", name: 'placa' },
+  { image: "assets/img/gallery/productos/tazas.png", name: 'tazas' },
+  // { image: "https://http2.mlstatic.com/D_NQ_NP_2X_859917-MLM53384513131_012023-F.webp", name: 'portaretratos' },
+  // { image: "https://ae01.alicdn.com/kf/S70032da91ae34fe8ada365675ea39fcd1/Plumas-de-sublimaci-n-de-8-piezas-con-envoltura-retr-ctil-Pluma-de-transferencia-de-calor.jpg_Q90.jpg_.webp", name: 'pluma' },
 ];
